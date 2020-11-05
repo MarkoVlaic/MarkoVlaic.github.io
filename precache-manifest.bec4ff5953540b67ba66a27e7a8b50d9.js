024568f46797bce6675ee45a685f28770f504703
MarkoVlaic <vlaic.marko@gmail.com>
2020-11-05 23:50:21 +0100

    Added production data

---
+++ precache-manifest.bec4ff5953540b67ba66a27e7a8b50d9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1404b5f366cbeddd30b34a6249dae6cc",
+    "url": "./MarkoVlaic.github.io/index.html"
+  },
+  {
+    "revision": "d5a010786a26f337cee5",
+    "url": "./MarkoVlaic.github.io/static/css/2.f4bde6ca.chunk.css"
+  },
+  {
+    "revision": "66e9125369519eff3753",
+    "url": "./MarkoVlaic.github.io/static/css/main.e5d84dde.chunk.css"
+  },
+  {
+    "revision": "d5a010786a26f337cee5",
+    "url": "./MarkoVlaic.github.io/static/js/2.bd26b512.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "./MarkoVlaic.github.io/static/js/2.bd26b512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e9125369519eff3753",
+    "url": "./MarkoVlaic.github.io/static/js/main.35c3f0ae.chunk.js"
+  },
+  {
+    "revision": "2ece61664bb2320ad470",
+    "url": "./MarkoVlaic.github.io/static/js/runtime-main.eceafeeb.js"
+  },
+  {
+    "revision": "11ff695d77fc45caaee0bae1225156a0",
+    "url": "./MarkoVlaic.github.io/static/media/babic-drzavno.11ff695d.jpg"
+  },
+  {
+    "revision": "fbfb93a2f06c0bc8d3e3672b943580ed",
+    "url": "./MarkoVlaic.github.io/static/media/hero.fbfb93a2.jpg"
+  },
+  {
+    "revision": "0b7b42af60b22f4372f05b90a4fe3988",
+    "url": "./MarkoVlaic.github.io/static/media/icons.0b7b42af.svg"
+  },
+  {
+    "revision": "74df92cb429ce20260dfa4b63cbdcda9",
+    "url": "./MarkoVlaic.github.io/static/media/icons.74df92cb.svg"
+  },
+  {
+    "revision": "b78e7998e8f63770472ab5b262f29467",
+    "url": "./MarkoVlaic.github.io/static/media/icons.b78e7998.svg"
+  },
+  {
+    "revision": "001d82511e0f905a18d460d368814a17",
+    "url": "./MarkoVlaic.github.io/static/media/ivan-cakovec.001d8251.jpg"
+  },
+  {
+    "revision": "489ccdbcadb0f17d6516fe5800bfc009",
+    "url": "./MarkoVlaic.github.io/static/media/ivan-liga.489ccdbc.jpg"
+  },
+  {
+    "revision": "9b268a2cd59521d46a2e8a38394934da",
+    "url": "./MarkoVlaic.github.io/static/media/ivan-tia-leon-nesto.9b268a2c.jpg"
+  },
+  {
+    "revision": "00729ae86452508fbc2dd8ed4be0078f",
+    "url": "./MarkoVlaic.github.io/static/media/leon-mat-drzavno.00729ae8.jpg"
+  },
+  {
+    "revision": "ab42544d3bb98dacfc6b452b7d7ad0ee",
+    "url": "./MarkoVlaic.github.io/static/media/leon-tibor-tia-martin.ab42544d.jpg"
+  },
+  {
+    "revision": "fa61871cc05bc1c9fdf1ce87712d8caa",
+    "url": "./MarkoVlaic.github.io/static/media/martin-drzavno.fa61871c.jpg"
+  },
+  {
+    "revision": "4c87e4ef8a258eaa6abf4d660ffefa7c",
+    "url": "./MarkoVlaic.github.io/static/media/martin-nesto.4c87e4ef.jpg"
+  },
+  {
+    "revision": "89ed8931b2337c856db58ecdc81b800e",
+    "url": "./MarkoVlaic.github.io/static/media/mat-drzavno.89ed8931.jpg"
+  },
+  {
+    "revision": "4ee4d1940b3fc387a04487434b44de7a",
+    "url": "./MarkoVlaic.github.io/static/media/matija-nesto.4ee4d194.jpg"
+  },
+  {
+    "revision": "f08a8fda4491e7cdbda7ad0bb43a0077",
+    "url": "./MarkoVlaic.github.io/static/media/tia-drzavno.f08a8fda.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.047d1c501e15d3e0bdc93203ab8b05db.js"
+  "./MarkoVlaic.github.io/precache-manifest.bec4ff5953540b67ba66a27e7a8b50d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./MarkoVlaic.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
